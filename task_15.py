20415e2b5249b8b56c16aff546fb54958ffeb65d
Владислав Я. Полищук <polischuk_vy@edu.kvantorium-pk.ru>
2020-03-05 17:08:13 +1000

    dd

--- task_15.py
+++ task_15.py
@@ -7,8 +7,34 @@ from pyrob.api import *
 def task_8_21():
     while True:
         if wall_is_above() and wall_is_on_the_left():
-            while not wall
-    pass
+            while not wall_is_on_the_right():
+                move_right()
+            while not wall_is_beneath():
+                move_down()
+            if wall_is_beneath() and wall_is_on_the_right():
+                break
+        if wall_is_above() and wall_is_on_the_right ():
+            while not wall_is_on_the_left():
+                move_left()
+            while not wall_is_beneath():
+                move_down()
+            if wall_is_beneath() and wall_is_on_the_left():
+                break
+        if wall_is_beneath() and wall_is_on_the_left():
+            while not wall_is_above():
+                move_up()
+            while not wall_is_on_the_right():
+                move_right()
+            if wall_is_above() and wall_is_on_the_right():
+                break
+        if wall_is_beneath() and wall_is_on_the_right():
+            while not wall_is_above():
+                move_up()
+            while not wall_is_on_the_left():
+                move_left()
+            if wall_is_above() and wall_is_on_the_left():
+                break
+pass
 
 
 if __name__ == '__main__':
